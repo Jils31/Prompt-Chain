4c6d1f1fd1840702563911addeeaeb0bd61c27e0
Jils31 <jilspatel21@gmail.com>
2025-10-24 14:53:28 +0530

    Deployment link changes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import cookieParser from "cookie-parser";
 const app = express()
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://prompt-chain-eight.vercel.app/'], 
+  origin: ['http://localhost:5173', 'https://prompt-chain-jnygy8oq1-jils-projects-ec28c228.vercel.app'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
